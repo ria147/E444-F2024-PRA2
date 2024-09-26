c6c96eaa558b9ccc9d8bcbdf18dff4abbec34b72
ria147 <62183694+ria147@users.noreply.github.com>
2024-09-25 22:20:11 -0400

    Example 3.5 - Adding navigation bar using Flask-Bootstrap, and Title with my name

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', my_name="Raisa")
 
 @app.route('/user/<name>')
 def user(name):
